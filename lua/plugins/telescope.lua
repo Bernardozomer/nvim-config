9d0c0247400c0af5f2364dbe130c840e5a7d75e4
Bernardozomer <bernardobarzottoz@gmail.com>
2023-03-16 13:55:24 -0300

    feat: add telescope key mappings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,8 +1,26 @@
 -- highly extensible fuzzy finder over lists
--- TODO: add keybindings, lsp-zero and project.nvim integration
 return {
 	'nvim-telescope/telescope.nvim',
 	tag = '0.1.1',
 	dependencies = { 'nvim-lua/plenary.nvim' },
-	config = true,
+	config = {
+		require('telescope').load_extension('projects')
+	},
+	keys = {
+		-- vim pickers
+		{ '<leader>fb', '<cmd>lua require(\'telescope.builtin\').buffers()<CR>' },
+		{ '<leader>ff', '<cmd>lua require(\'telescope.builtin\').find_files()<CR>' },
+		{ '<leader>fg', '<cmd>lua require(\'telescope.builtin\').live_grep()<CR>' },
+		{ '<leader>fh', '<cmd>lua require(\'telescope.builtin\').help_tags()<CR>' },
+		{ '<leader>fm', '<cmd>lua require(\'telescope.builtin\').marks()<CR>' },
+		{ '<leader>fo', '<cmd>lua require(\'telescope.builtin\').oldfiles()<CR>' },
+		{ '<leader>fr', '<cmd>lua require(\'telescope.builtin\').registers()<CR>' },
+		-- lsp pickers
+		{ '<leader>gds', '<cmd>lua require(\'telescope.builtin\').lsp_document_symbols()<CR>' },
+		{ '<leader>gci', '<cmd>lua require(\'telescope.builtin\').lsp_incoming_calls()<CR>' },
+		{ '<leader>gco', '<cmd>lua require(\'telescope.builtin\').lsp_outgoing_calls()<CR>' },
+		{ '<leader>gr', '<cmd>lua require(\'telescope.builtin\').lsp_references()<CR>' },
+		-- extension: projects
+		{ '<leader>ep', '<cmd>lua require(\'telescope\').extensions.projects.projects()<CR>' },
+	},
 }
