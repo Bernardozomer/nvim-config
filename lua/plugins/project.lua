96ed25cd6878048a9f08e5efb2ab2cdf179ccbad
Bernardozomer <bernardobarzottoz@gmail.com>
2023-06-22 10:40:26 -0300

    fix: add telescope as dependency for project.nvim

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -2,6 +2,9 @@
 -- automatically cd to project directory and telescope integration
 return {
 	'ahmedkhalf/project.nvim',
+    dependencies = {
+		'nvim-telescope/telescope.nvim',
+	},
 	config = function ()
 		require('project_nvim').setup()
 	end
